8d68bcecef32f61f6a7f1dc5c89d0d24418afac3
Jason Azevedo <jasonazevedo03@gmail.com>
2020-11-15 16:47:06 +0200

    Created CaesarCipher class and implemented most of the program

---
+++ src/CaesarCipher.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CaesarCipher
+{
+    class CaesarCipher
+    {
+        public char key { get; set; }
+        
+        private static Dictionary<char, byte> alphabetPositions = new Dictionary<char, byte>
+        {
+            {'a', 0 },  {'b', 1 },  {'c', 2 },
+            {'d', 3 },  {'e', 4 },  {'f', 5 },
+            {'g', 6 },  {'h', 7 },  {'i', 8 },
+            {'j', 9 },  {'k', 10 }, {'l', 11 },
+            {'m', 12 }, {'n', 13 }, {'o', 14 },
+            {'p', 15 }, {'q', 16 }, {'r', 17 },
+            {'s', 18 }, {'t', 19 }, {'u', 20 },
+            {'v', 21 }, {'w', 22 }, {'x', 23 },
+            {'y', 24 }, {'z', 25 }
+        };
+
+        public string Encrypt(string plainText)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Decrypt(string cipherText)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- src/Program.cs
+++ src/Program.cs
@@ -6,7 +6,42 @@ namespace CaesarCipher
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("This program encrypts stuff!");
+            CaesarCipher caesarCipher = new CaesarCipher();
+            string result = ""; // Empty assignment prevents compiler error at end of file 
+
+            Console.Write("Encrypt [y] Decrypt [n]: ");
+            char mode = Char.ToLower(Console.ReadKey().KeyChar);
+
+            Console.Write("\nKey Char: ");
+            char key = Char.ToLower(Console.ReadKey().KeyChar);
+
+            if (key == 13 || key == 0)
+            {
+                Console.WriteLine("Please enter a correct key value!");
+                return;
+            }
+            else { caesarCipher.key = key; }
+
+            if (mode == 'y')
+            {
+                //Get the text to encrypt and encrypt it!
+                Console.Write("\nPlain Text: ");
+                string plainText = Console.ReadLine();
+
+                result = caesarCipher.Encrypt(plainText);
+            }
+            else if (mode == 'n')
+            {
+                //Get the text to decrypt and then decrypt it!
+                Console.Write("\nCipher Text: ");
+                string cipherText = Console.ReadLine();
+
+                result = caesarCipher.Decrypt(cipherText);
+            }
+            else { Console.WriteLine("[ERROR]: Please enter correct value"); }
+
+            //Output the result
+            Console.WriteLine($"Result:\n{result}");
         }
     }
 }
